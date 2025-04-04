ed790a44144c80e129af51a7e6f96a6c06e42b77
Nico <nicolasaddamo1@gmail.com>
2025-04-04 16:25:12 -0300

    added new folder and dtos

--- backend/src/modules/matches/match.controller.ts
+++ backend/src/modules/matches/match.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+// src/match/match.controller.ts
+import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import { MatchService } from './match.service';
+import { CreateMatchDto } from './dto/create-match.dto';
+import { UpdateMatchDto } from './dto/update-match.dto';
 
-@Controller('match')
-export class MatchController {}
+@Controller('matches')
+export class MatchController {
+    constructor(private readonly matchService: MatchService) { }
+
+    @Post()
+    create(@Body() dto: CreateMatchDto) {
+        return this.matchService.create(dto);
+    }
+
+    @Get()
+    findAll() {
+        return this.matchService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.matchService.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateMatchDto) {
+        return this.matchService.update(id, dto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.matchService.remove(id);
+    }
+}
