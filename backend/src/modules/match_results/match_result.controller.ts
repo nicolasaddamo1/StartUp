ec0f605745813939ee3a9d9bcb3e6fed7497d99a
Nico <nicolasaddamo1@gmail.com>
2025-04-04 16:31:56 -0300

    modified controller

--- backend/src/modules/match_results/match_result.controller.ts
+++ backend/src/modules/match_results/match_result.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+// src/modules/match_results/match_result.controller.ts
+import {
+    Controller, Post, Get, Body, Param, Put, Delete,
+} from '@nestjs/common';
+import { MatchResultService } from './match_result.service';
+import { CreateMatchResultDto } from './dto/create-match_result.dto';
+import { UpdateMatchResultDto } from './dto/update-match_result.dto';
 
-@Controller('match-result')
-export class MatchResultController {}
+@Controller('match-results')
+export class MatchResultController {
+    constructor(private readonly matchResultService: MatchResultService) { }
+
+    @Post()
+    create(@Body() dto: CreateMatchResultDto) {
+        return this.matchResultService.create(dto);
+    }
+
+    @Get()
+    findAll() {
+        return this.matchResultService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.matchResultService.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateMatchResultDto) {
+        return this.matchResultService.update(id, dto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.matchResultService.remove(id);
+    }
+}
