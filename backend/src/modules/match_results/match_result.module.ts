1147bf9afc5cca2981c61b67aec962dbae018485
Nico <nicolasaddamo1@gmail.com>
2025-04-04 16:32:41 -0300

    modified module

--- backend/src/modules/match_results/match_result.module.ts
+++ backend/src/modules/match_results/match_result.module.ts
@@ -1,9 +1,15 @@
+// src/modules/match_results/match_result.module.ts
 import { Module } from '@nestjs/common';
-import { MatchResultController } from './match_result.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { MatchResultService } from './match_result.service';
+import { MatchResultController } from './match_result.controller';
+import { MatchResult } from 'src/entity/match-result.entity';
+import { Player } from 'src/entity/player.entity';
+import { Match } from 'src/entity/match.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([MatchResult, Player, Match])],
   controllers: [MatchResultController],
-  providers: [MatchResultService]
+  providers: [MatchResultService],
 })
-export class MatchResultModule {}
+export class MatchResultModule { }
