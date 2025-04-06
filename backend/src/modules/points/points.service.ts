16522903d54d20a6e7616b0aaf8ecb74be4f548a
Nico <nicolasaddamo1@gmail.com>
2025-04-06 18:20:06 -0300

    modified points service

--- backend/src/modules/points/points.service.ts
+++ backend/src/modules/points/points.service.ts
@@ -1,4 +1,42 @@
+// src/modules/points/points.service.ts
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreatePointDto } from './dto/create-point.dto';
+import { UpdatePointDto } from './dto/update-point.dto';
+import { Point } from './entities/point.entity';
+import { Player } from '../players/entities/player.entity';
+import { Match } from '../matches/entities/match.entity';
 
 @Injectable()
-export class PointsService {}
+export class PointsService {
+    constructor(
+        @InjectRepository(Point)
+        private pointRepository: Repository<Point>,
+    ) { }
+
+    create(createPointDto: CreatePointDto) {
+        return this.pointRepository.save(createPointDto);
+    }
+
+    findAll() {
+        return this.pointRepository.find({
+            relations: ['player', 'match'],
+        });
+    }
+
+    findOne(id: string) {
+        return this.pointRepository.findOne({
+            where: { id },
+            relations: ['player', 'match'],
+        });
+    }
+
+    update(id: string, updatePointDto: UpdatePointDto) {
+        return this.pointRepository.update(id, updatePointDto);
+    }
+
+    remove(id: string) {
+        return this.pointRepository.delete(id);
+    }
+}
