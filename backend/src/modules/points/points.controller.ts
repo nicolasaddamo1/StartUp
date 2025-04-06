24ccffe3177df5a4663cf3a4d0bd0cd6487be851
Nico <nicolasaddamo1@gmail.com>
2025-04-06 18:23:34 -0300

    modified point controller.

--- backend/src/modules/points/points.controller.ts
+++ backend/src/modules/points/points.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+// src/modules/points/points.controller.ts
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PointsService } from './points.service';
+import { CreatePointDto } from './dto/create-point.dto';
+import { UpdatePointDto } from './dto/update-point.dto';
 
 @Controller('points')
-export class PointsController {}
+export class PointsController {
+    constructor(private readonly pointsService: PointsService) { }
+
+    @Post()
+    create(@Body() createPointDto: CreatePointDto) {
+        return this.pointsService.create(createPointDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.pointsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.pointsService.findOne(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updatePointDto: UpdatePointDto) {
+        return this.pointsService.update(id, updatePointDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.pointsService.remove(id);
+    }
+}
