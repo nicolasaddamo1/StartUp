cc2a6553ad7eb4f7b6df80342dac32114fd023f7
Nico <nicolasaddamo1@gmail.com>
2025-04-06 18:24:21 -0300

    modified point module

--- backend/src/modules/points/points.module.ts
+++ backend/src/modules/points/points.module.ts
@@ -1,9 +1,13 @@
+// src/modules/points/points.module.ts
 import { Module } from '@nestjs/common';
-import { PointsController } from './points.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PointsService } from './points.service';
+import { PointsController } from './points.controller';
+import { Point } from 'src/entity/points.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Point])],
   controllers: [PointsController],
-  providers: [PointsService]
+  providers: [PointsService],
 })
-export class PointsModule {}
+export class PointsModule { }
