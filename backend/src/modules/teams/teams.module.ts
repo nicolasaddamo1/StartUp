5e9334f45c6782f2ac00a7651cacc3c06c47bdc8
Nico <nicolasaddamo1@gmail.com>
2025-04-06 18:01:03 -0300

    added service

--- backend/src/modules/teams/teams.module.ts
+++ backend/src/modules/teams/teams.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TeamsController } from './teams.controller';
+import { TeamService } from './team.service';
 
 @Module({
-  controllers: [TeamsController]
+  controllers: [TeamsController],
+  providers: [TeamService]
 })
 export class TeamsModule {}
