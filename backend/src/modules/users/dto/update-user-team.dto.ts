bae3f52ae3a287bd3e46bf0c3229f7bd829345c1
Nico <nicolasaddamo1@gmail.com>
2025-04-08 11:12:10 -0300

    modified update user dto

--- backend/src/modules/users/dto/update-user-team.dto.ts
+++ backend/src/modules/users/dto/update-user-team.dto.ts
@@ -1,4 +1,23 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserTeamDto } from './create-user-team.dto';
+// dto/update-usuario.dto.ts
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsOptional, MinLength } from 'class-validator';
 
-export class UpdateUserTeamDto extends PartialType(CreateUserTeamDto) { }
+export class UpdateUsuarioDto {
+    @ApiPropertyOptional({ example: 'Nuevo Nombre' })
+    @IsOptional()
+    nombre?: string;
+
+    @ApiPropertyOptional({ example: 'nuevo@email.com' })
+    @IsOptional()
+    @IsEmail({}, { message: 'El email debe ser válido' })
+    email?: string;
+
+    @ApiPropertyOptional({ example: 'NuevaContraseña123!' })
+    @IsOptional()
+    @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+    password?: string;
+
+    @ApiPropertyOptional({ example: 'http://ejemplo.com/avatar.jpg' })
+    @IsOptional()
+    avatar_url?: string;
+}
