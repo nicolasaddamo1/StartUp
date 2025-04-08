c231f8de447e093a509e152b56cf4268dd9a63e6
Nico <nicolasaddamo1@gmail.com>
2025-04-08 11:02:32 -0300

    fixed controller errors

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,16 +1,14 @@
 // auth.controller.ts
 import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LocalAuthGuard } from './guards/local-auth.guard';
-import { RegisterDto } from './dto/register.dto';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { RegisterDto } from './dto/register.dto';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
-    @UseGuards(LocalAuthGuard)
     @Post('login')
     @ApiOperation({ summary: 'Iniciar sesión' })
     @ApiResponse({ status: 200, description: 'Login exitoso' })
