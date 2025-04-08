674e60f58e3d21a2a5613932a7fe0d9e61cd31c7
Nicolas <nicolasaddamo1@gmail.com>
2025-04-07 22:54:20 -0300

    added market entity

---
+++ backend/src/entity/market.entiy.ts
@@ -0,0 +1,29 @@
+import { Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Torneo } from "./tournaments.entity";
+
+export class Mercado {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    
+    @Column({ type: 'timestamp' })
+    fecha_apertura: Date;
+    
+    @Column({ type: 'timestamp' })
+    fecha_cierre: Date;
+    
+    @Column({ default: true })
+    activo: boolean;
+    
+    @Column({ nullable: true })
+    descripcion: string;
+    
+    @Column({ type: 'enum', enum: ['inicial', 'regular', 'final'], default: 'regular' })
+    tipo: string;
+    
+    @ManyToOne(() => Torneo)
+    @JoinColumn({ name: 'torneo_id' })
+    torneo: Torneo;
+    
+    @Column({ nullable: true })
+    torneo_id: string;
+  }
