eea0b6827a3e492d5bc683a632732ed6062ed570
Nicolas <nicolasaddamo1@gmail.com>
2025-04-07 22:45:49 -0300

    added fecha

---
+++ backend/src/entity/fecha.entity.ts
@@ -0,0 +1,41 @@
+import { Column, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Torneo } from "./tournaments.entity";
+
+export enum EstadoFecha {
+    PENDIENTE = 'pendiente',
+    EN_CURSO = 'en_curso',
+    FINALIZADA = 'finalizada'
+  }
+  
+  export class Fecha {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    
+    @Column()
+    numero: number;
+    
+    @Column({ type: 'enum', enum: EstadoFecha, default: EstadoFecha.PENDIENTE })
+    estado: EstadoFecha;
+    
+    @Column({ type: 'timestamp' })
+    fecha_inicio: Date;
+    
+    @Column({ type: 'timestamp' })
+    fecha_fin: Date;
+    
+    @Column({ nullable: true })
+    descripcion: string;
+    
+    @ManyToOne(() => Torneo, torneo => torneo.fechas)
+    @JoinColumn({ name: 'torneo_id' })
+    torneo: Torneo;
+    
+    @Column()
+    torneo_id: string;
+    
+    @OneToMany(() => Puntuacion, puntuacion => puntuacion.fecha)
+    puntuaciones: Puntuacion[];
+    
+    @OneToMany(() => Partido, partido => partido.fecha)
+    partidos: Partido[];
+  }
