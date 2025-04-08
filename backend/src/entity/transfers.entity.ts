124465c9882f27f38306acc2367bda220c955f70
Nicolas <nicolasaddamo1@gmail.com>
2025-04-07 22:56:06 -0300

    added transfers

---
+++ backend/src/entity/transfers.entity.ts
@@ -0,0 +1,43 @@
+import { Column, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Equipo } from "./team.entity";
+import { Mercado } from "./market.entiy";
+import { Jugador } from "./player.entity";
+
+export class Transferencia {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    
+    @ManyToOne(() => Equipo)
+    @JoinColumn({ name: 'equipo_origen_id' })
+    equipo_origen: Equipo;
+    
+    @Column({ nullable: true })
+    equipo_origen_id: string;
+    
+    @ManyToOne(() => Equipo)
+    @JoinColumn({ name: 'equipo_destino_id' })
+    equipo_destino: Equipo;
+    
+    @Column()
+    equipo_destino_id: string;
+    
+    @ManyToOne(() => Jugador)
+    @JoinColumn({ name: 'jugador_id' })
+    jugador: Jugador;
+    
+    @Column()
+    jugador_id: string;
+    
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    monto: number;
+    
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    fecha: Date;
+    
+    @ManyToOne(() => Mercado)
+    @JoinColumn({ name: 'mercado_id' })
+    mercado: Mercado;
+    
+    @Column({ nullable: true })
+    mercado_id: string;
+  }
