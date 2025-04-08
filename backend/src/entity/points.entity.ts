ac098c86b3075076741d54a9a9f56ecb11e6a514
Nicolas <nicolasaddamo1@gmail.com>
2025-04-07 22:47:11 -0300

    modified points

--- backend/src/entity/points.entity.ts
+++ backend/src/entity/points.entity.ts
@@ -1,27 +1,58 @@
-// src/modules/points/entities/point.entity.ts
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
-import { Player } from './player.entity';
-import { Match } from './match.entity';
+import { Column, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { Jugador } from "./player.entity";
+import { Fecha } from "./fecha.entity";
 
-@Entity('points')
-export class Point {
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
-
-    @ManyToOne(() => Player)
-    @JoinColumn({ name: 'player_id' })
-    player: Player;
-
-    @ManyToOne(() => Match)
-    @JoinColumn({ name: 'match_id' })
-    match: Match;
-
-    @Column()
-    score: number;
-
-    @Column({ name: 'is_player_of_the_match', default: false })
-    isPlayerOfTheMatch: boolean;
-
-    @Column({ name: 'is_player_of_the_week', default: false })
-    isPlayerOfTheWeek: boolean;
-}
+export class Puntuacion {
+    @PrimaryColumn()
+    jugador_id: string;
+    
+    @PrimaryColumn()
+    fecha_id: string;
+    
+    @Column({ type: 'decimal', precision: 5, scale: 2, default: 0 })
+    puntos_base: number;
+    
+    @Column({ default: 0 })
+    goles: number;
+    
+    @Column({ default: 0 })
+    asistencias: number;
+    
+    @Column({ default: 0 })
+    tarjetas_amarillas: number;
+    
+    @Column({ default: 0 })
+    tarjetas_rojas: number;
+    
+    @Column({ default: 0 })
+    penales_atajados: number;
+    
+    @Column({ default: 0 })
+    penales_errados: number;
+    
+    @Column({ default: 0 })
+    arcos_en_cero: number;
+    
+    @Column({ default: 0 })
+    goles_en_contra: number;
+    
+    @Column({ default: false })
+    mvp: boolean;
+    
+    @Column({ default: false })
+    jugador_fecha: boolean;
+    
+    @Column({ default: 0 })
+    minutos_jugados: number;
+    
+    @Column({ type: 'decimal', precision: 5, scale: 2, default: 0 })
+    puntos_totales: number;
+    
+    @ManyToOne(() => Jugador, jugador => jugador.puntuaciones)
+    @JoinColumn({ name: 'jugador_id' })
+    jugador: Jugador;
+    
+    @ManyToOne(() => Fecha, fecha => fecha.puntuaciones)
+    @JoinColumn({ name: 'fecha_id' })
+    fecha: Fecha;
+  }
