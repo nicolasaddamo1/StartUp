1977c7ad73a5fab41f58d6947be8782f2c91d92d
Nico <nicolasaddamo1@gmail.com>
2025-04-09 17:46:43 -0300

    added entities to module

--- backend/src/modules/club/club.module.ts
+++ backend/src/modules/club/club.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClubController } from './club.controller';
 import { ClubService } from './club.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Club } from 'src/entity/club.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Club])],
   controllers: [ClubController],
   providers: [ClubService]
 })
-export class ClubModule {}
+export class ClubModule { }
--- backend/src/modules/players/player.module.ts
+++ backend/src/modules/players/player.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PlayerController } from './player.controller';
 import { PlayerService } from './player.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Jugador } from 'src/entity/player.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Jugador])],
   controllers: [PlayerController],
   providers: [PlayerService]
 })
-export class PlayerModule {}
+export class PlayerModule { }
