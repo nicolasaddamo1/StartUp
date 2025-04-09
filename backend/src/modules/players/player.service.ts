aebf92d82fef8229456575b93e3b5ed9e0e0ad16
Nico <nicolasaddamo1@gmail.com>
2025-04-09 16:49:16 -0300

    ready service

--- backend/src/modules/players/player.service.ts
+++ backend/src/modules/players/player.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Jugador } from 'src/entity/player.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PlayerService {}
+export class PlayerService {
+    constructor(
+        @InjectRepository(Jugador) private playerRepository: Repository<Jugador>,
+    ) { }
+    async findAllPlayers(): Promise<Jugador[]> {
+        const jugadores = await this.playerRepository.find();
+        if (!jugadores) throw new Error('No se encontraron jugadores');
+        return jugadores
+    }
+
+    async findPlayerById(id: string): Promise<Jugador> {
+        const jugador = await this.playerRepository.findOne({ where: { id } });
+        if (!jugador) throw new Error('Jugador no encontrado');
+        return jugador;
+    }
+
+    async createPlayer(player: Jugador): Promise<Jugador> {
+        const newPlayer = this.playerRepository.create(player);
+        return await this.playerRepository.save(newPlayer);
+    }
+
+    async updatePlayer(id: string, player: Partial<Jugador>): Promise<Jugador> {
+        const existingPlayer = await this.playerRepository.findOne({ where: { id } });
+        if (!existingPlayer) throw new Error('Jugador no encontrado');
+        Object.assign(existingPlayer, player);
+        return await this.playerRepository.save(existingPlayer);
+    }
+
+    async deletePlayer(id: string): Promise<void> {
+        const result = await this.playerRepository.delete(id);
+        if (result.affected === 0) throw new Error('Jugador no encontrado');
+    }
+
+}
