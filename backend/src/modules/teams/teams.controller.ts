af7b94985077893d650e80e73fdfb252b4eddff6
Nicolas <nicolasaddamo1@gmail.com>
2025-04-14 09:02:42 -0300

    changes on controller

--- backend/src/modules/teams/teams.controller.ts
+++ backend/src/modules/teams/teams.controller.ts
@@ -2,16 +2,18 @@
 import { Controller, Put, Param, Body, Post } from '@nestjs/common';
 import { FormationEnum } from 'src/formations/formations.constants';
 import { EquipoService } from './team.service';
+import { CreateUserTeamDto } from './folder/user-team.dto';
 
 @Controller('equipos')
 export class EquipoController {
   constructor(private readonly equipoService: EquipoService) {}
 
   @Post('team')
-  async createTeam( @Body('name')
-    name:string
+  async createTeam( @Body('team')
+    team:CreateUserTeamDto
   ){
-    return this.equipoService.createSquad(name);
+    if(!team.teamName || !team.userId)throw new Error('Campos Obligatorios.')
+    return this.equipoService.createSquad(team);
   }
   @Put(':id/formation')
   async updateFormation(
