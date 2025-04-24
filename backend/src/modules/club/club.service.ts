332d351a278dd9dbb32a0cb263be4ce6db23b0a7
Nico <nicolasaddamo1@gmail.com>
2025-04-24 19:24:53 -0300

    feat: idk

--- backend/src/modules/club/club.service.ts
+++ backend/src/modules/club/club.service.ts
@@ -4,7 +4,7 @@ import { Club } from 'src/entity/club.entity';
 import { DeleteResult, Repository } from 'typeorm';
 import { CreateClubDto } from './dto/create-club.entity';
 import { UpdateClubDto } from './dto/update-club.entity';
-
+import * as fs from 'fs';
 interface Player {
     id: number;
     name: string;
@@ -24,27 +24,31 @@ export class ClubService {
         private clubRepository: Repository<Club>,
     ) { }
 
+
+
     async traerclubesAtravesdeApi() {
         var axios = require('axios');
 
         var config = {
             method: 'get',
-            url: 'https://v3.football.api-sports.io/players?league=128&season=2023',
+            url: process.env.URL,
             headers: {
                 'x-rapidapi-key': process.env.API_KEY,
                 'x-rapidapi-host': 'v3.football.api-sports.io'
             }
         };
 
-        axios(config)
-            .then(function (response) {
-                console.log(JSON.stringify(response.data));
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+        try {
+            const response = await axios(config);
+            // Guardar la respuesta en un archivo JSON
+            fs.writeFileSync('clubes.json', JSON.stringify(response.data, null, 2));
+            return response.data;
+        } catch (error) {
+            // Guardar el error en un archivo JSON de errores
+            fs.writeFileSync('error.json', JSON.stringify(error, null, 2));
+            throw error;
+        }
     }
-
     async findAll(): Promise<Club[]> {
         return this.clubRepository.find();
     }
