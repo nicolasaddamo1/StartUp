39d6d92b689e765159b9c7a6e3d7dba242bd2ec7
Nico <nicolasaddamo1@gmail.com>
2025-05-08 16:35:14 -0300

    feat: added dashboard

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,4 +1,13 @@
 // Tablero con estadísticas 
-export default function Dashboard(){
-    
+export default function Dashboard() {
+    return (
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold leading-tight mt-0 mb-2">
+                Dashboard
+            </h1>
+            <p className="text-gray-600 text-md">
+                Esta es una vista de ejemplo, reemplaza con tu contenido
+            </p>
+        </div>
+    );
 }
